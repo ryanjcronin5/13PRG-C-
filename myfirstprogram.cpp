578604b0a5c9ac96114afcffa0eb8ba7c21a3e2d
Ryan Cronin <ryanjcronin5@gmail.com>
2023-02-20 12:26:31 +1300

    Created testing playground

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
+class MyClass {
+public:
+    int myNum;
+    string myString;
+};
+
 int main() {
-    
+    MyClass myObj;
+    myObj.myNum = 15;
+    myObj.myString = "Some Text";
+
+    cout << myObj.myNum << "\n";
+    cout << myObj.myString;
+
     return 0;
 }
